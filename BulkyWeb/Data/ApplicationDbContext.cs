6d8270fa01197329d701b7ec8ffd48300121c153
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-07-25 19:35:35 -0600

    Seed Category Table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace BulkyWeb.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category {  Id = 1, Name = "Action", DisplyOrder = 1},
+                new Category {  Id = 2, Name = "Scifi", DisplyOrder = 2},
+                new Category { Id = 3, Name = "History", DisplyOrder = 3 }
+                );
+        }
     }
 }
