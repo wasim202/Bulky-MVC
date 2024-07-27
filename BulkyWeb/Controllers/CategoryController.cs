ac49d0f3402dea118fb737313d356fac6bb504ae
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-07-27 09:21:20 -0600

    Create partial view - temp data

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -11,11 +11,13 @@ namespace BulkyWeb.Controllers
         {
             _db = db;
         }
+
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
         public IActionResult Create()
         {
             return View();
@@ -31,11 +33,13 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
 
         }
+
         public IActionResult Edit(int? id)
         {
             if(id == null || id == 0)
@@ -60,6 +64,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -90,6 +95,7 @@ namespace BulkyWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
         }
