af95e9870431f68f6c64c6f0c460e9e0a0932768
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-07-26 23:43:29 -0600

    Get Category Details to Edit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -34,7 +34,41 @@ namespace BulkyWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View();
-            
+
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplyOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order and Category Name can NOT be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
     }
 }
