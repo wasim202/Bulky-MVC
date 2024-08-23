5ab7f25fcc332190c073188bce24ed3a2d15c9a0
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-08-23 07:52:22 -0600

    Update Product Custom Code in the Product Repository

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -20,7 +20,27 @@ namespace BulkyBook.DataAccess.Repository
 
         public void update(Product obj)
         {
-           _db.Products.Update(obj);
+           //_db.Products.Update(obj);
+
+            //if we wanna be explicit about what need to be updated:
+             
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Price = obj.Price;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Author = obj.Author;
+                objFromDb.ISBN = obj.ISBN;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
         }
     }
 }
