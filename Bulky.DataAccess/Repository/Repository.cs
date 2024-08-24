2d561f0a77bcc4d0c9d57151bff820ee8f488c95
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-08-24 08:36:37 -0600

    Loading Navigation Properties - Add Include Properties to the IRepository  - Add Category.Name to the Product Index

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -17,24 +17,42 @@ namespace BulkyBook.DataAccess.Repository
         public Repository(ApplicationDbContext db)
         {
             _db = db;
-            //_db.Categories == dbSet
             this.dbSet = _db.Set<T>();
+            //_db.Categories == dbSet
+            _db.Products.Include(u => u.Category).Include(u=>u.CategoryId);
+
         }
         public void Add(T entity)
         {
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
-            return query.FirstOrDefault();
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                //to make sure if there are more than one include properties then we can add them as a comma separated list
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+                return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
            IQueryable<T> query = dbSet;
+                if (!string.IsNullOrEmpty(includeProperties))
+                {
+                    //to make sure if there are more than one include properties then we can add them as a comma separated list
+                    foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        query = query.Include(includeProp);
+                    }
+                }
             return query.ToList(); 
         }
 
