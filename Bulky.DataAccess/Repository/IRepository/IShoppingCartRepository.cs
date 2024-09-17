bcc502c9e106525c957dbe0b3811b7ed5d8740b3
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-09-16 21:40:40 -0600

    Add Shopping Cart To Repository

---
+++ Bulky.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IReposirtory<ShoppingCart>
+    {
+        void update(ShoppingCart obj);
+        
+    }
+}
--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
         IProductRepository Product { get; }
         ICompanyRepository Company { get; }
+        IShoppingCartRepository ShoppingCart { get; }
         void save();
     }
 }
