bb428e0f07456df19b0187928e84bb5da92386ac
wasim202 <65715473+wasim202@users.noreply.github.com>
2024-09-23 20:45:35 -0600

    Get OrderTotal in Shopping Cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -29,7 +30,31 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 includeProperties:"Product")
             };
 
+            foreach( var cart in ShoppingCartVM.ShoppingCartList )
+            {
+                cart.Price = GetPriceBasedOnQuantity( cart );
+                ShoppingCartVM.Ordertotal += (cart.Price * cart.Count);
+
+            }
+
             return View(ShoppingCartVM);
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                    return shoppingCart.Product.Price100;
+            }
         }
     }
 }
